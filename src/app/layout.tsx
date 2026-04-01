28e1681dc16c9bd8fc77b49fc5b368cade29635f
roland <aminiroland01@gmail.com>
2026-04-01 17:58:28 +0300

    added icon png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "The Borderless Identity",
-  description: "Cross-border e-commerce platform",
+  description: " The borderless Identity is more than a clothing brand it's a movement, a lifestyle, and a statement. We believe in breaking down barriers and embracing individuality. Our designs are inspired by the idea of a world without borders, where everyone can express themselves freely and authentically. Join us on this journey to redefine fashion and celebrate diversity with The Borderless Identity.",
+  icons: {
+    icon: "/icon.png", // Chemin relatif à partir du dossier public ou géré par Next.js
+    apple: "/icon.png", // Optionnel : pour les appareils iOS
+  },
 };
 
 export default function RootLayout({
